dd3270909a33c83267f3cd5ef72cd07c9a86f273
Eduardo Moura Zampirolli <ra281154@honduras.lab.ic.unicamp.br>
2025-03-13 11:56:22 -0300

    Criei outro arquivo teste2 para coletar as informações do site do senado

---
+++ teste2.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://escriba.camara.leg.br/escriba-servicosweb/html/75070'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    html_content = response.text
+    soup = BeautifulSoup(html_content, 'html.parser')
+    links = soup.find_all('a')
+
+    for link in links:
+        href = link.get('href')
+        text = link.text.strip()
+        print(f"Link Text: {text}, URL: {href}")
+else:
+    print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+print(soup)
