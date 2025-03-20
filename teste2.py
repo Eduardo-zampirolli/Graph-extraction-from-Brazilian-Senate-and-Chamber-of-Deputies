0fb86d621406de6b17c5ddaca2bca77b29d6add0
Eduardo Moura Zampirolli <ra281154@ash.lab.ic.unicamp.br>
2025-03-20 14:00:37 -0300

    Inicio de criar .txt para o Senado
    Comecei a utilizar os arquivos teste2.py e bb.py para testar a navegação e a busca no html do site do senado, consegui separar todos os sujeitos citados, mas não consegui pegar os textos, vou tentar separar a árvore em subárvores que contém as falas dos políticos

--- teste2.py
+++ teste2.py
@@ -1,7 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-url = 'https://escriba.camara.leg.br/escriba-servicosweb/html/75070'
+def sujeito(txt):
+    return(txt[:6] == 'A SRA.' or txt[:5] == 'O SR.')
+
+
+codigo = 75070
+url = 'https://escriba.camara.leg.br/escriba-servicosweb/html/'+f'{codigo}'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
@@ -16,11 +22,15 @@ if response.status_code == 200:
     for link in links:
         href = link.get('href')
         text = link.text.strip()
-        #print(f"Link Text: {text}, URL: {href}")
+        if href!= None and sujeito(text):
+            print(f"Link Text: {text}, URL: {href}")
 else:
     print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
 '''busca: html -> body -> div -> div(id="contentEncontro") -> table (id = tabelaQuadros) 
 -> tbody -> tr (quartos) -> td "justificado" -> segundo div -> span (vao conter os paragrafos) 
 -> <b> -> <a> (vão conter os nomes dos sujeitos)
 '''
-print(soup.find_all('a'))
+#print(soup.find_all('a'))
+print(soup.prettify)
+
+     
